74f5b41b06bfdc7982f3141631a4f5ce2048e5f0
Kapi-Github <120816506+Kapi-Github@users.noreply.github.com>
2024-09-13 11:20:13 +0200

    polimorfizm 13.09.2024

---
+++ prog_obiektowe/Polimorfizm/Polimorfizm/Program.cs
@@ -0,0 +1,110 @@
+﻿namespace Polimorfizm
+{
+    internal class Program
+    {
+        class Figura
+        {
+            protected string nazwa;
+            public Figura(string nazwa)
+            {
+                this.nazwa = nazwa;
+            }
+            public virtual void Wyswietl()
+            {
+                Console.WriteLine($"Figura geometryczna: {nazwa}");
+            }
+        }
+
+        class Kolo : Figura
+        {
+            double promien;
+            public Kolo(string nazwa, double promien): base(nazwa)
+            { 
+                this.promien = promien;
+            }
+            public double Pole() 
+            {
+                return Math.PI * Math.Pow(promien, 2);
+            }
+            public override void Wyswietl()
+            {
+                base.Wyswietl();
+                Console.WriteLine($"Pole figury: {Pole()}");
+            }
+        }
+
+        class Kula : Kolo
+        {
+            double promien;
+            public Kula(string nazwa, double promien) : base(nazwa, promien)
+            {
+                this.promien = promien;
+            }
+            public double Objetosc() 
+            {
+                return (4 / 3) * Math.PI * Math.Pow(promien, 3); 
+            }
+            public double Pole() 
+            { 
+                return 4 * Math.PI * Math.Pow(this.promien, 2);
+            }
+            public override void Wyswietl()
+            {
+                base.Wyswietl();
+                Console.WriteLine($"Objętość: {Objetosc()}");  
+            }
+        }
+
+        class Stozek : Kolo
+        {
+            double wysokosc, promien;
+            public Stozek(double wysokosc, double promien)
+            {
+                this.wysokosc = wysokosc;
+                this.promien = promien;
+            }
+            public double Objetosc() 
+            {
+                return (1 / 3) * Math.PI * Math.Pow(promien, 2) * wysokosc;
+            }
+            public double Pole()
+            {
+                return Math.PI * promien * (promien + Math.Sqrt(Math.Pow(promien, 2) + Math.Pow(wysokosc, 2)));
+            }
+            public override void Wyswietl()
+            {
+                base.Wyswietl();
+                Console.WriteLine($"Objętość: {Objetosc()}");
+            }
+        }
+
+        class Walec : Kolo
+        {
+            double wysokosc, promien;
+            public Walec(double wysokosc, double promien) 
+            {
+                this.wysokosc = wysokosc;
+                this.promien = promien;
+            }
+            public double Objetosc()
+            {
+                return Math.PI * Math.Pow(promien, 2) * wysokosc;
+            }
+            public double Pole()
+            {
+                return 2 * Math.PI * promien * (promien + wysokosc);
+            }
+            public override void Wyswietl()
+            {
+                base.Wyswietl();
+                Console.WriteLine($"Objętość: {Objetosc()}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Figura[] obiekty = new Figura[7];
+            obiekty[0] = new Figura("");
+        }
+    }
+}
---
+++ proj_oprogramowania/Wyszukiwanie_naiwne/Wyszukiwanie_naiwne/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Wyszukiwanie_naiwne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            string s = "";
+
+            for(int i = 0; i < 60; i++)
+            {
+                s += (char)rand.Next('A', 'Z' + 1);
+            }
+
+            s = s.ToLower();
+
+            Console.WriteLine($"Tekst: {s}");
+            Console.WriteLine("Wpisz szukany łańcuch znaków");
+            string p = Console.ReadLine();
+            int znaleziona = -1;
+            int ileznaleziono = 0;
+
+            int k;
+            for (k = 0; k < s.Length; k++)
+            {
+                for(int j = 0; j < p.Length; j++)
+                {
+                    if ((k + j < s.Length) && p[j] == s[k + j])
+                    {
+                        ileznaleziono++;
+                    }
+                }
+                if(ileznaleziono == p.Length)
+                {
+                    znaleziona = k;
+                    break;
+                }
+                else
+                {
+                    ileznaleziono = 0;
+                    k += ileznaleziono;
+                }
+            }
+
+            if(znaleziona >= 0)
+            {
+                Console.WriteLine($"Znaleziono na pozycji {znaleziona}");
+            }
+            else
+            {
+                Console.WriteLine("Nie znaleziono");
+            }
+        }
+    }
+}
